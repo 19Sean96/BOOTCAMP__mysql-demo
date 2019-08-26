5a894adb72e3839c9eefa780a458a7ffb8a88ded
Sean A <1996.sean.alexander@gmail.com>
2019-08-25 22:55:10 -0700

    config env

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,29 @@
 // Set up MySQL connection.
+require("dotenv").config();
+
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: process.env.DB_LOCAL_USER,
+        password: process.env.DB_LOCAL_PASS,
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
